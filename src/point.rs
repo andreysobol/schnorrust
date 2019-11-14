f92efb96604d908bcfcd9eb59be3bf2f51c1c7e1
Andrey Sobol <baskerville@andreysobol.com>
2019-11-14 23:44:36 +0200

    add Infinity

--- src/point.rs
+++ src/point.rs
@@ -1,13 +1,25 @@
 extern crate num_bigint;
 use num_bigint::{BigUint};
 
-pub struct Point{
-    x: BigUint,
-    y: BigUint,
+pub enum Point{
+    Infinity,
+    ExistingPoint{
+        x: BigUint,
+        y: BigUint,
+    }
 }
 
 pub fn equal_points(p1: Point, p2: Point) -> bool {
-    (p1.x == p2.x) && (p1.y == p2.y)
+    match p1 {
+        Point::Infinity => match p2 {
+            Point::Infinity => true,
+            _ => false,
+        },
+        Point::ExistingPoint {x:x1, y:y1} => match p2 {
+            Point::Infinity => false,
+            Point::ExistingPoint {x:x2, y:y2} => (x1 == x2) && (y1 == y2),
+        },
+    }
 }
 
 pub fn sum_ponts(p1: Point, p2: Point) -> Point {
