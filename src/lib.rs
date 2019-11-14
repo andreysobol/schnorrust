708fec6d2c75545da0698b461b632353578f7cff
Andrey Sobol <baskerville@andreysobol.com>
2019-11-14 19:47:14 +0200

    add num-bigint

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
-#[cfg(test)]
+extern crate num_bigint;
 
+#[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn biguint_check(){
+        let a = num_bigint::BigUint::parse_bytes(b"FFFF", 16).unwrap();
+        let b = num_bigint::BigUint::parse_bytes(b"01", 16).unwrap();
+        let c = a + b;
+        assert_eq!(num_bigint::BigUint::parse_bytes(b"10000", 16).unwrap(), c);
     }
 }
