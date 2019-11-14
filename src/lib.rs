034195d41b199feae25eeede8690b323b613cd8e
Andrey Sobol <baskerville@andreysobol.com>
2019-11-14 20:23:40 +0200

    add test number more than 32bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate num_bigint;
 
+mod secp256k1;
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -13,5 +15,13 @@ mod tests {
         let b = num_bigint::BigUint::parse_bytes(b"01", 16).unwrap();
         let c = a + b;
         assert_eq!(num_bigint::BigUint::parse_bytes(b"10000", 16).unwrap(), c);
+    }
+
+    #[test]
+    fn biguint_check_max(){
+        let a = num_bigint::BigUint::parse_bytes(b"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", 16).unwrap();
+        let b = num_bigint::BigUint::parse_bytes(b"01", 16).unwrap();
+        let c = a + b;
+        assert_eq!(num_bigint::BigUint::parse_bytes(b"10000000000000000000000000000000000000000000000000000000000000000", 16).unwrap(), c);
     }
 }
