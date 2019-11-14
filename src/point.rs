c910d05f8510c357cfd05fb13053a3f653d8bd84
Andrey Sobol <baskerville@andreysobol.com>
2019-11-14 23:16:48 +0200

    equal_points

--- src/point.rs
+++ src/point.rs
@@ -5,3 +5,11 @@ pub struct Point{
     x: BigUint,
     y: BigUint,
 }
+
+pub fn equal_ponts(p1: Point, p2: Point) -> bool {
+    (p1.x == p2.x) && (p1.y == p2.y)
+}
+
+pub fn sum_ponts(p1: Point, p2: Point) -> Point {
+    p1
+}
