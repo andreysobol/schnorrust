9fc668e32ff3ed1030a774e9b5e08301cb8fb4de
Andrey Sobol <baskerville@andreysobol.com>
2019-11-14 20:24:48 +0200

    add 4 const

---
+++ src/secp256k1.rs
@@ -0,0 +1,9 @@
+extern crate num_bigint;
+use num_bigint::{BigUint};
+
+pub fn secp256k1_params() {
+    let p: BigUint = BigUint::parse_bytes(b"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16).unwrap();
+    let n: BigUint = BigUint::parse_bytes(b"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16).unwrap();
+    let Gx: BigUint = BigUint::parse_bytes(b"79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 16).unwrap();
+    let Gy: BigUint = BigUint::parse_bytes(b"483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", 16).unwrap();
+}
