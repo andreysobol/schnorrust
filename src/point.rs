bf9f05c5dcccb3fc325ba19fde727ca131f5f553
Andrey Sobol <baskerville@andreysobol.com>
2019-11-15 00:29:13 +0200

    add Point::Infinity condition to sum

--- src/point.rs
+++ src/point.rs
@@ -50,8 +50,8 @@ pub fn sum_ponts(p1: Point, p2: Point) -> Point {
     if is_infinity(&p2){
         return p1
     }
-    if get_x(&p1) == get_x(&p2){
-        return p1
+    if (get_x(&p1) == get_x(&p2)) && (get_y(&p1) != get_y(&p2)) {
+        return Point::Infinity
     }
     return p1
 }
