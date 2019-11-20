1ad5287723ddbf18f1ffa19e54445394e1b8ce80
Andrey Sobol <baskerville@andreysobol.com>
2019-11-20 05:04:16 +0200

    add static TAG

--- src/message_hash.rs
+++ src/message_hash.rs
@@ -2,8 +2,10 @@ extern crate easy_hash;
 
 use self::easy_hash::{Sha256, Hasher, HashResult};
 
+static TAG:&[u8] = b"BIPSchnorr";
+
 pub fn message_hash_with_tag(msg: &[u8]) -> [u8; 32]{
-    let th = Sha256::hash("BIPSchnorr".as_bytes());
+    let th = Sha256::hash(TAG);
     let mut d = th.to_vec();
     d.extend(&th);
     d.extend(msg);
