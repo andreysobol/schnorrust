64aef7f1658f05dce86fb16ad0fb8a29a51a15c5
Andrey Sobol <baskerville@andreysobol.com>
2019-11-20 04:58:35 +0200

    add message to hash

--- src/message_hash.rs
+++ src/message_hash.rs
@@ -4,4 +4,8 @@ use self::easy_hash::{Sha256, Hasher, HashResult};
 
 pub fn message_hash_with_tag(msg: &[u8]){
     let th = Sha256::hash("BIPSchnorr".as_bytes());
+    let mut d = th.to_vec();
+    d.extend(&th);
+    d.extend(msg);
+    let res = Sha256::hash(&d);
 }
