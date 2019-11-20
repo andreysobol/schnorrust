ba71773e323f215047692cfee63414dfde06bf20
Andrey Sobol <baskerville@andreysobol.com>
2019-11-20 11:13:43 +0200

    presign

--- src/sign.rs
+++ src/sign.rs
@@ -1,7 +1,22 @@
 extern crate num_bigint;
 
 use num_bigint::{BigInt, ToBigInt};
+use secp256k1::{secp256k1_params};
+use point::{mul_points, Point};
 
 pub fn sign(secret: BigInt, message: [u8; 32]){
+    let sparam = secp256k1_params();
+    let n = sparam.n;
+    let g = Point::ExistingPoint{
+        x: sparam.Gx,
+        y: sparam.Gy,
+    };
 
+    if (((0.to_bigint().unwrap()) < secret) && (secret < n)){ 
+        panic!("The secret not in range 1..n-1.");
+    }
+
+    let p = mul_points(g, secret);
+    
+    //realsecret = 
 }
