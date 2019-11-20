704bfcfbdd32b979c9089e8116059b9ec2ca8bf2
Andrey Sobol <baskerville@andreysobol.com>
2019-11-20 04:59:40 +0200

    return result

--- src/message_hash.rs
+++ src/message_hash.rs
@@ -2,10 +2,11 @@ extern crate easy_hash;
 
 use self::easy_hash::{Sha256, Hasher, HashResult};
 
-pub fn message_hash_with_tag(msg: &[u8]){
+pub fn message_hash_with_tag(msg: &[u8]) -> [u8; 32]{
     let th = Sha256::hash("BIPSchnorr".as_bytes());
     let mut d = th.to_vec();
     d.extend(&th);
     d.extend(msg);
     let res = Sha256::hash(&d);
+    res
 }
