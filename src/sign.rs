144fcbcccf9946304946e61b22b49ce282fdae7f
Andrey Sobol <baskerville@andreysobol.com>
2019-11-21 03:59:02 +0200

    part of sign

--- src/sign.rs
+++ src/sign.rs
@@ -1,8 +1,9 @@
 extern crate num_bigint;
 
-use num_bigint::{BigInt, ToBigInt};
+use num_bigint::{BigInt, ToBigInt, Sign};
 use secp256k1::{secp256k1_params};
 use point::{mul_points, Point, is_infinity, square};
+use message_hash::message_hash_with_tag;
 
 pub fn sign(secret: BigInt, message: [u8; 32]){
     let sparam = secp256k1_params();
@@ -11,8 +12,9 @@ pub fn sign(secret: BigInt, message: [u8; 32]){
         x: sparam.Gx,
         y: sparam.Gy,
     };
+    let zero = 0.to_bigint().unwrap();
 
-    if (((0.to_bigint().unwrap()) < secret) && (secret < n)){ 
+    if (zero < secret) && (secret < n) { 
         panic!("The secret not in range 1..n-1.");
     }
 
@@ -21,6 +23,22 @@ pub fn sign(secret: BigInt, message: [u8; 32]){
     let realsecret = if square(&p){
         secret
     } else {
-        n - secret
+        &n - secret
     };
+
+    let secret_tuple =  realsecret.to_bytes_be();
+    let mut secret_vector = secret_tuple.1;
+
+    secret_vector.resize_with(32, || { 0 });
+    let mut vector_for_hashing = secret_vector;
+    vector_for_hashing.extend(&message);
+
+    let hashed_data = message_hash_with_tag(&vector_for_hashing);
+    let hashed_bigint = BigInt::from_bytes_be(Sign::Plus, &hashed_data);
+    let k = hashed_bigint % n;
+
+    if k==zero {
+        panic!("k=0. Unbelievable but it's true");
+    }
+
 }
