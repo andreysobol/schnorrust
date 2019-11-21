8438de5ac5148837023404a78102351ecd38a6c6
Andrey Sobol <baskerville@andreysobol.com>
2019-11-21 09:44:43 +0200

    sign and Signature

--- src/sign.rs
+++ src/sign.rs
@@ -2,10 +2,15 @@ extern crate num_bigint;
 
 use num_bigint::{BigInt, ToBigInt, Sign};
 use secp256k1::{secp256k1_params};
-use point::{mul_points, Point, is_infinity, square};
+use point::{mul_points, Point, is_infinity, square, get_x};
 use message_hash::message_hash_with_tag;
 
-pub fn sign(secret: BigInt, message: [u8; 32]){
+pub struct Signature {
+    r: BigInt,
+    s: BigInt,
+}
+
+pub fn sign(secret: BigInt, message: [u8; 32]) -> Signature {
     let sparam = secp256k1_params();
     let n = sparam.n;
     let g = Point::ExistingPoint{
@@ -35,10 +40,48 @@ pub fn sign(secret: BigInt, message: [u8; 32]){
 
     let hashed_data = message_hash_with_tag(&vector_for_hashing);
     let hashed_bigint = BigInt::from_bytes_be(Sign::Plus, &hashed_data);
-    let k = hashed_bigint % n;
+    let k = hashed_bigint % &n;
 
     if k==zero {
         panic!("k=0. Unbelievable but it's true");
     }
 
+    let anothersparam = secp256k1_params();
+    let anotherg = Point::ExistingPoint{
+        x: anothersparam.Gx,
+        y: anothersparam.Gy,
+    };
+    let r = mul_points(anotherg, &k);
+
+    let realk = if square(&r){
+        k
+    } else {
+        &n - k
+    };
+
+    let rx = get_x(&r);
+    let rx_tuple = rx.to_bytes_be();
+    let mut rx_vector = rx_tuple.1;
+    rx_vector.resize_with(32, || { 0 });
+
+    let px = get_x(&p);
+    let px_tuple = px.to_bytes_be();
+    let mut px_vector = px_tuple.1;
+    px_vector.resize_with(32, || { 0 });
+
+    let mut second_hash_data = Vec::new();
+    second_hash_data.extend(&rx_vector);
+    second_hash_data.extend(&px_vector);
+    second_hash_data.extend(&message);
+
+    let second_hashed_data = message_hash_with_tag(&second_hash_data);
+    let second_hashed_bigint = BigInt::from_bytes_be(Sign::Plus, &second_hashed_data);
+    let e = second_hashed_bigint % &n;
+
+    let s = (realk + e * realsecret) % &n;
+
+    Signature{
+        r: rx.clone(),
+        s: s,
+    }
 }
