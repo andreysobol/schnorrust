fb1d6dc3007623c93e04680e0eab526fd24763ab
Andrey Sobol <baskerville@andreysobol.com>
2019-11-22 12:49:01 +0200

    add 2 serialisatiom methods

--- src/signature.rs
+++ src/signature.rs
@@ -4,3 +4,13 @@ pub struct Signature {
     pub r: BigInt,
     pub s: BigInt,
 }
+
+impl Signature {
+    fn r_to_bytes(&self) -> Vec<u8>{
+        self.r.to_bytes_be().1
+    }
+
+    fn s_to_bytes(&self) -> Vec<u8>{
+        self.r.to_bytes_be().1
+    }
+}
