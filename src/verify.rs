4089e98ba6d84922c582b5c5c2ef12a72f3e50c9
Andrey Sobol <baskerville@andreysobol.com>
2019-11-25 09:46:43 +0200

    verify p not infinity

--- src/verify.rs
+++ src/verify.rs
@@ -1,9 +1,13 @@
 use num_bigint::{BigInt, ToBigInt, Sign};
 
 use signature::Signature;
-use point::Point;
+use point::{Point, equal_points};
 
 pub fn sign(public: BigInt, message: [u8; 32], signature: Signature) -> bool {
     let p = Point::from_x(public);
+
+    if equal_points(&p, &Point::Infinity) {
+        return false;
+    }
     true
 }
