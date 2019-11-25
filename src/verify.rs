e8e1d0b8cc3c4aad6ec331b9546bd8bd3ce9a213
Andrey Sobol <baskerville@andreysobol.com>
2019-11-25 10:03:10 +0200

    sig r and sig s >= p and n

--- src/verify.rs
+++ src/verify.rs
@@ -2,12 +2,23 @@ use num_bigint::{BigInt, ToBigInt, Sign};
 
 use signature::Signature;
 use point::{Point, equal_points};
+use secp256k1::{secp256k1_params};
 
 pub fn sign(public: BigInt, message: [u8; 32], signature: Signature) -> bool {
-    let p = Point::from_x(public);
 
-    if equal_points(&p, &Point::Infinity) {
+    let sparam = secp256k1_params();
+    let p = sparam.p;
+    let n = sparam.n;
+
+    let point = Point::from_x(public);
+
+    if equal_points(&point, &Point::Infinity) {
         return false;
     }
+
+    if (signature.r >= p || signature.s >= n){
+        return false;
+    }
+
     true
 }
