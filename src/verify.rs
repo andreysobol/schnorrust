ab9f7370a62e15b00769502469e94f3683738610
Andrey Sobol <baskerville@andreysobol.com>
2019-11-25 09:36:18 +0200

    Point::from_x(public); in verify

--- src/verify.rs
+++ src/verify.rs
@@ -1,7 +1,9 @@
 use num_bigint::{BigInt, ToBigInt, Sign};
 
 use signature::Signature;
+use point::Point;
 
 pub fn sign(public: BigInt, message: [u8; 32], signature: Signature) -> bool {
+    let p = Point::from_x(public);
     true
 }
