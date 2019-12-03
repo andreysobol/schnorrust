97cb61644364d638335b886eddcc1b9b65ee654f
Andrey Sobol <baskerville@andreysobol.com>
2019-12-03 13:04:50 +0200

    for history

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,13 @@
-use num_bigint::{BigInt, BigUint, Sign};
+use num_bigint::{BigInt, BigUint, Sign, ToBigInt};
 
 fn parse_bigint_from_u_bytes(bytes: &[u8]) -> BigInt {
     let bui = BigUint::parse_bytes(bytes, 16).unwrap();
     let i = BigInt::from_biguint(Sign::Plus, bui);
     i
+}
+
+#[test]
+fn test_parser() {
+    let i = parse_bigint_from_u_bytes(b"FF");
+    assert_eq!(&i, &255.to_bigint().unwrap());
 }
