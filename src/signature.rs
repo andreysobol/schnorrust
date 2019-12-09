6ff612d14df5f56d746e8af963303778c2fa016a
Andrey Sobol <baskerville@andreysobol.com>
2019-12-09 04:22:57 +0200

    add pub to 2 fn inside Signature

--- src/signature.rs
+++ src/signature.rs
@@ -6,13 +6,13 @@ pub struct Signature {
 }
 
 impl Signature {
-    fn r_to_bytes(&self) -> Vec<u8>{
+    pub fn r_to_bytes(&self) -> Vec<u8>{
         let mut rv = self.r.to_bytes_be().1;
         rv.resize_with(32, || { 0 });
         rv
     }
 
-    fn s_to_bytes(&self) -> Vec<u8>{
+    pub fn s_to_bytes(&self) -> Vec<u8>{
         let mut sv = self.s.to_bytes_be().1;
         sv.resize_with(32, || { 0 });
         sv
