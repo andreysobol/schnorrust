a0744a16b6783bc3644d37bc0735bdbd9c73e564
Andrey Sobol <baskerville@andreysobol.com>
2019-12-12 01:09:56 +0200

    add 2 test to tool

--- src/tool.rs
+++ src/tool.rs
@@ -10,3 +10,22 @@ pub fn unsigned_modulo(i: &BigInt, m: &BigInt) -> BigInt{
 
     r
 }
+
+
+#[test]
+fn positive_unsigned_modulo(){
+    let i = 4.to_bigint().unwrap();
+    let m = 3.to_bigint().unwrap();
+
+    let r = unsigned_modulo(&i, &m);
+    assert_eq!(r, 1.to_bigint().unwrap());
+}
+
+#[test]
+fn negative_unsigned_modulo(){
+    let i = (-7).to_bigint().unwrap();
+    let m = 3.to_bigint().unwrap();
+
+    let r = unsigned_modulo(&i, &m);
+    assert_eq!(r, 2.to_bigint().unwrap());
+}
